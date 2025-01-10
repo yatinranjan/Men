ee9a9747cca9c7d8bdded2fd05ba508622940f73
Jitendra Kumar Ram <154624917+yatinranjan@users.noreply.github.com>
2025-01-10 19:20:58 +0530

    express complete

--- backend/express-project/index.js
+++ backend/express-project/index.js
@@ -5,4 +5,7 @@ let app = express()
 app.get("/", (req, res) => {
     res.send({status:1, message:"Home Page API"})
 })
+app.get("/news", (req, res) => {
+    res.send({status:1, message:"news Page API"})
+})
 app.listen("8000")
